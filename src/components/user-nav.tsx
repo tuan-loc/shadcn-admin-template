e89da7ed06ff76d2faf64bb8b703ea61964a972f
tuanlocvocong <tuanlocvocong@gmail.com>
2026-03-07 16:31:13 +0700

    update label style

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -25,9 +25,9 @@ export function UserNav() {
       <DropdownMenuContent className='w-56' align='end' forceMount>
         <DropdownMenuLabel className='font-normal'>
           <div className='flex flex-col space-y-1'>
-            <p className='text-sm font-medium leading-none'>satnaing</p>
+            <p className='text-sm font-medium leading-none'>tuanloc</p>
             <p className='text-xs leading-none text-muted-foreground'>
-              satnaingdev@gmail.com
+              tuanlocdev@gmail.com
             </p>
           </div>
         </DropdownMenuLabel>
