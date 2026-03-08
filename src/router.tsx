5ac493e051c2d61a8edda898682bc7763826a7d6
tuanlocvocong <tuanlocvocong@gmail.com>
2026-03-08 21:25:07 +0700

    add sign-up page

--- src/router.tsx
+++ src/router.tsx
@@ -6,6 +6,7 @@ const Dashboard = lazy(() => import('@/pages/dashboard'))
 const SignIn = lazy(() => import('@/pages/auth/sign-in'))
 const SignIn2 = lazy(() => import('@/pages/auth/sign-in-2'))
 const ForgotPassword = lazy(() => import('@/pages/auth/forgot-password'))
+const SignUp = lazy(() => import('@/pages/auth/sign-up'))
 
 export default function Router() {
   return (
@@ -14,6 +15,7 @@ export default function Router() {
         <Route path='/' element={<Dashboard />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-in-2' element={<SignIn2 />} />
+        <Route path='/sign-up' element={<SignUp />} />
         <Route path='/forgot-password' element={<ForgotPassword />} />
       </Routes>
     </Suspense>
