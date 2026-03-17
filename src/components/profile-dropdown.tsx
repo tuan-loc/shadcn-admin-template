43cb125d2d824ce12d14280bb0e05a1e22679766
tuanlocvocong <tuanlocvocong@gmail.com>
2026-03-17 14:04:17 +0700

    upgrade Tailwind CSS to v4

--- src/components/profile-dropdown.tsx
+++ src/components/profile-dropdown.tsx
@@ -26,8 +26,8 @@ export function ProfileDropdown() {
       <DropdownMenuContent className='w-56' align='end' forceMount>
         <DropdownMenuLabel className='font-normal'>
           <div className='flex flex-col space-y-1'>
-            <p className='text-sm font-medium leading-none'>tuanloc</p>
-            <p className='text-xs leading-none text-muted-foreground'>
+            <p className='text-sm leading-none font-medium'>tuanloc</p>
+            <p className='text-muted-foreground text-xs leading-none'>
               tuanlocdev@gmail.com
             </p>
           </div>
