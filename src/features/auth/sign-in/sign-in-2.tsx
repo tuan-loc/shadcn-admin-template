39e7e7cf235d8feec935cbf58c5cfdaf05db60be
tuanlocvocong <tuanlocvocong@gmail.com>
2026-03-24 09:18:37 +0700

    replace inline svg logo with logo component

--- src/features/auth/sign-in/sign-in-2.tsx
+++ src/features/auth/sign-in/sign-in-2.tsx
@@ -1,3 +1,4 @@
+import { Logo } from '@/assets/logo'
 import ViteLogo from '@/assets/vite.svg'
 import { UserAuthForm } from './components/user-auth-form'
 
@@ -7,19 +8,8 @@ export function SignIn2() {
       <div className='bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-e'>
         <div className='absolute inset-0 bg-zinc-900' />
         <div className='relative z-20 flex items-center text-lg font-medium'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='2'
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            className='me-2 h-6 w-6'
-          >
-            <path d='M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3' />
-          </svg>
-          Shadcn Admin
+          <Logo className='me-2' />
+          <h1 className='text-xl font-medium'>Shadcn Admin</h1>
         </div>
 
         <img
