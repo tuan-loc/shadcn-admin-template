9f5f557a364348beb455cba1dca6caec0a29101f
tuanlocvocong <tuanlocvocong@gmail.com>
2026-03-30 13:35:06 +0700

    update prettier tailwind config and formatting

--- src/components/profile-dropdown.tsx
+++ src/components/profile-dropdown.tsx
@@ -31,9 +31,9 @@ export function ProfileDropdown() {
         <DropdownMenuContent className='w-56' align='end' forceMount>
           <DropdownMenuLabel className='font-normal'>
             <div className='flex flex-col gap-1.5'>
-              <p className='text-sm leading-none font-medium'>satnaing</p>
-              <p className='text-muted-foreground text-xs leading-none'>
-                satnaingdev@gmail.com
+              <p className='text-sm leading-none font-medium'>tuanloc</p>
+              <p className='text-xs leading-none text-muted-foreground'>
+                tuanlocdev@gmail.com
               </p>
             </div>
           </DropdownMenuLabel>
